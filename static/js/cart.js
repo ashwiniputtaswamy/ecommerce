4af3b6389aef931582f9c3246106f5f19d037d00
ashwiniputtaswamy <ashwiniputtaswamy05@gmail.com>
2023-06-15 12:13:22 +0530

    eco

--- static/js/cart.js
+++ static/js/cart.js
@@ -7,6 +7,35 @@ for(var i = 0; i < updateBtns.length; i++){
         console.log('productId:', productId, 'action:', action)
 
         console.log('USER:', user)
+        if(user == 'AnonymousUser'){
+            console.log('Not Logged In')
+        }
+        else{
+            updateUserOrder(productId, action)
+        }
+    })
+}
+
+
+function updateUserOrder(productId, action){
+    console.log('user has logged in, sending data..')
+
+    var url = '/update_item/'
+    fetch(url, {
+        method: 'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'X-CSRFToken':csrftoken,
+        },
+        body:JSON.stringify({'productId': productId, 'action': action})
+    })
+
+    .then((response) =>{
+        return response.json()
+    })
+
+    .then((data) =>{
+        console.log('data:', data)
     })
 }
 
--- store/views.py
+++ store/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import *
+import json
 
 
 def store(request):
     products = Product.objects.all()
-    context = {'products':products}
+    context = {'products': products}
     return render(request, 'store/store.html', context)
 
 
@@ -17,7 +19,7 @@ def cart(request):
         items = []
         order = {'get_cart_total': 0, 'get_items_total': 0}
 
-    context = {'items':items, 'order':order}
+    context = {'items': items, 'order': order}
     return render(request, 'store/cart.html', context)
 
 
@@ -30,5 +32,15 @@ def checkout(request):
         items = []
         order = {'get_cart_total': 0, 'get_items_total': 0}
 
-    context = {'items':items, 'order':order}
+    context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
+
+
+def updateItem(request):
+    data = json.loads(request.data)
+    productId = data['productId']
+    action = data['action']
+
+    print('Action:', action)
+    print('productId:', productId)
+    return JsonResponse('Item was added', safe=False)
